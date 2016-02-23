9850a55bae811932b9f61ec86b7f6caa7ddf7dbb
Sunny-guangge <18611021761@163.com>
2016-02-23 18:33:41 +0800

    UIViewCategory

--- WSGCategory/Category/UIView+WSGAdd.h
+++ WSGCategory/Category/UIView+WSGAdd.h
@@ -10,4 +10,21 @@
 
 @interface UIView (WSGAdd)
 
+@property (nonatomic) CGFloat left;
+@property (nonatomic) CGFloat right;
+@property (nonatomic) CGFloat top;
+@property (nonatomic) CGFloat bottom;
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+@property (nonatomic) CGFloat centerX;
+@property (nonatomic) CGFloat centerY;
+@property (nonatomic) CGPoint origin;
+@property (nonatomic) CGSize  size;
+
+#pragma mark - 圆角
+- (void)setRoundedCorners:(UIRectCorner)corners radius:(CGSize)size;
+
+#pragma mark - 以递归的方式遍历(查找)subview
+- (UIView*)findViewRecursively:(BOOL(^)(UIView* subview, BOOL* stop))recurse;
+
 @end
