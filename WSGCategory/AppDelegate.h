1b5f7a2f67ef59934f54b5f2c37ed10ba2541e63
Sunny-guangge <18611021761@163.com>
2016-02-23 15:59:10 +0800

    初始化工程

---
+++ WSGCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSGCategory
+//
+//  Created by user on 16/2/23.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
